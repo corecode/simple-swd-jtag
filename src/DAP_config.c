c66669154ec80991ba51157c4ba5544f8870e111
Simon Schubert <2@0x2c.org>
2014-11-19 02:43:22 +0100

    start DAP port for mchck

---
+++ src/DAP_config.c
@@ -0,0 +1,174 @@
+#include <mchck.h>
+#include "DAP_config.h"
+
+enum {
+        PIN_TCK_SWCLK = PIN_PTD5,
+        PIN_TMS_SWDIO = PIN_PTC6,
+        PIN_TDI = PIN_PTD6,
+        PIN_TDO = PIN_PTD7,
+        PIN_nRESET = PIN_PTE0,
+
+        PIN_LED_RED = PIN_PTB0,
+        PIN_LED_GREEN = PIN_PTB1,
+};
+
+void
+PORT_JTAG_SETUP(void)
+{
+        gpio_write(PIN_TCK_SWCLK, GPIO_HIGH);
+        gpio_write(PIN_TMS_SWDIO, GPIO_HIGH);
+        gpio_write(PIN_TDI, GPIO_HIGH);
+        gpio_write(PIN_nRESET, GPIO_HIGH);
+
+        gpio_dir(PIN_TCK_SWCLK, GPIO_OUTPUT);
+        gpio_dir(PIN_TMS_SWDIO, GPIO_OUTPUT);
+        gpio_dir(PIN_TDI, GPIO_OUTPUT);
+        gpio_dir(PIN_nRESET, GPIO_OUTPUT);
+}
+
+void
+PORT_SWD_SETUP(void)
+{
+        gpio_write(PIN_TCK_SWCLK, GPIO_HIGH);
+        gpio_write(PIN_TMS_SWDIO, GPIO_HIGH);
+        gpio_write(PIN_nRESET, GPIO_HIGH);
+
+        gpio_dir(PIN_TCK_SWCLK, GPIO_OUTPUT);
+        gpio_dir(PIN_TMS_SWDIO, GPIO_OUTPUT);
+        gpio_dir(PIN_TDI, GPIO_DISABLE);
+        gpio_dir(PIN_nRESET, GPIO_OUTPUT);
+}
+
+void
+PORT_OFF(void)
+{
+        gpio_dir(PIN_TCK_SWCLK, GPIO_DISABLE);
+        gpio_dir(PIN_TMS_SWDIO, GPIO_DISABLE);
+        gpio_dir(PIN_TDI, GPIO_DISABLE);
+        gpio_dir(PIN_nRESET, GPIO_DISABLE);
+}
+
+uint32_t
+PIN_SWCLK_TCK_IN(void)
+{
+        return (gpio_read(PIN_TCK_SWCLK));
+}
+
+void
+PIN_SWCLK_TCK_SET(void)
+{
+        gpio_write(PIN_TCK_SWCLK, GPIO_HIGH);
+}
+
+void
+PIN_SWCLK_TCK_CLR(void)
+{
+        gpio_write(PIN_TCK_SWCLK, GPIO_LOW);
+}
+
+uint32_t
+PIN_SWDIO_TMS_IN(void)
+{
+        return (gpio_read(PIN_TMS_SWDIO));
+}
+
+void
+PIN_SWDIO_TMS_SET(void)
+{
+        gpio_write(PIN_TMS_SWDIO, GPIO_HIGH);
+}
+
+void
+PIN_SWDIO_TMS_CLR(void)
+{
+        gpio_write(PIN_TMS_SWDIO, GPIO_LOW);
+}
+
+uint32_t
+PIN_SWDIO_IN(void)
+{
+        return (gpio_read(PIN_TMS_SWDIO));
+}
+
+void
+PIN_SWDIO_OUT(uint32_t bit)
+{
+        gpio_write(PIN_TMS_SWDIO, bit);
+}
+
+void
+PIN_SWDIO_OUT_ENABLE(void)
+{
+        gpio_dir(PIN_TMS_SWDIO, GPIO_OUTPUT);
+}
+
+void
+PIN_SWDIO_OUT_DISABLE(void)
+{
+        gpio_dir(PIN_TMS_SWDIO, GPIO_INPUT);
+}
+
+uint32_t
+PIN_TDI_IN(void)
+{
+        return (gpio_read(PIN_TDI));
+}
+
+void
+PIN_TDI_OUT(uint32_t bit)
+{
+        gpio_write(PIN_TDI, bit);
+}
+
+uint32_t
+PIN_TDO_IN(void)
+{
+        return (gpio_read(PIN_TDO));
+}
+
+uint32_t
+PIN_nRESET_IN(void)
+{
+        return (gpio_read(PIN_nRESET));
+}
+
+void
+PIN_nRESET_OUT(uint32_t bit)
+{
+        gpio_write(PIN_nRESET, bit);
+}
+
+void
+LED_CONNECTED_OUT(uint32_t bit)
+{
+        gpio_write(PIN_LED_GREEN, bit);
+}
+
+void
+LED_RUNNING_OUT(uint32_t bit)
+{
+        gpio_write(PIN_LED_RED, !bit);
+}
+
+void
+DAP_SETUP(void)
+{
+        pin_mode(PIN_TCK_SWCLK, PIN_MODE_RESET);
+        pin_mode(PIN_TMS_SWDIO, PIN_MODE_RESET);
+        pin_mode(PIN_TDI, PIN_MODE_RESET);
+        pin_mode(PIN_TDO, PIN_MODE_RESET);
+        pin_mode(PIN_nRESET, PIN_MODE_RESET | PIN_MODE_OPEN_DRAIN_ON | PIN_MODE_PULLUP);
+
+        gpio_dir(PIN_LED_RED, GPIO_OUTPUT);
+        gpio_dir(PIN_LED_GREEN, GPIO_OUTPUT);
+        gpio_dir(PIN_TDO, GPIO_INPUT);
+
+        gpio_write(PIN_LED_RED, GPIO_LOW);
+        gpio_write(PIN_LED_GREEN, GPIO_LOW);
+}
+
+uint32_t
+RESET_TARGET(void)
+{
+        return (0);
+}
---
+++ src/DAP_config.h
@@ -0,0 +1,38 @@
+#include <stdint.h>
+
+#define CPU_CLOCK 48000000
+#define IO_PORT_WRITE_CYCLES 2
+#define DAP_SWD 1
+#define DAP_JTAG 1
+#define DAP_JTAG_DEV_CNT 8
+#define DAP_DEFAULT_PORT 1
+#define DAP_DEFAULT_SWJ_CLOCK 1000000
+#define DAP_PACKET_SIZE 64
+#define DAP_PACKET_COUNT 64
+#define TARGET_DEVICE_FIXED 0
+
+void PORT_JTAG_SETUP(void);
+void PORT_SWD_SETUP(void);
+void PORT_OFF(void);
+uint32_t PIN_SWCLK_TCK_IN(void);
+void PIN_SWCLK_TCK_SET(void);
+void PIN_SWCLK_TCK_CLR(void);
+uint32_t PIN_SWDIO_TMS_IN(void);
+void PIN_SWDIO_TMS_SET(void);
+void PIN_SWDIO_TMS_CLR(void);
+uint32_t PIN_SWDIO_IN(void);
+void PIN_SWDIO_OUT(uint32_t bit);
+void PIN_SWDIO_OUT_ENABLE(void);
+void PIN_SWDIO_OUT_DISABLE(void);
+uint32_t PIN_TDI_IN(void);
+void PIN_TDI_OUT(uint32_t bit);
+uint32_t PIN_TDO_IN(void);
+uint32_t PIN_nRESET_IN(void);
+void PIN_nRESET_OUT(uint32_t bit);
+void LED_CONNECTED_OUT(uint32_t bit);
+void LED_RUNNING_OUT(uint32_t bit);
+void DAP_SETUP(void);
+uint32_t RESET_TARGET(void);
+
+#define PIN_nTRST_IN() (0)
+#define PIN_nTRST_OUT(x) /**/
